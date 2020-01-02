e5ab58bce89921f2e54ea2c89121015d04f0e2b0
fernando <fernandofitilan@hotmail.com>
2020-01-02 12:24:58 -0300

    upando

--- server.js
+++ server.js
@@ -16,96 +16,90 @@ const mc = mysql.createConnection({
     database: 'node_task_demo'
 });
  
-// connect to database
+
 mc.connect();
  
-// default route
+
 app.get('/', function (req, res) {
-    return res.send({ error: true, message: 'hello' })
+    return res.send({ error: true, message: 'Olá amigo' })
 });
  
-// Retrieve all todos 
+
 app.get('/todos', function (req, res) {
     mc.query('SELECT * FROM tasks', function (error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results, message: 'Todos list.' });
+        return res.send({ error: false, data: results, message: 'Todos itens.' });
     });
 });
- 
-// Search for todos with ‘bug’ in their name
+
 app.get('/todos/search/:keyword', function (req, res) {
     let keyword = req.params.keyword;
     mc.query("SELECT * FROM tasks WHERE task LIKE ? ", ['%' + keyword + '%'], function (error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results, message: 'Todos search list.' });
+        return res.send({ error: false, data: results, message: 'Pesquisa por nome.' });
     });
 });
- 
-// Retrieve todo with id 
+
 app.get('/todo/:id', function (req, res) {
  
     let task_id = req.params.id;
  
     mc.query('SELECT * FROM tasks where id=?', task_id, function (error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results[0], message: 'Todos list.' });
+        return res.send({ error: false, data: results[0], message: 'Lista do banco.' });
     });
  
 });
- 
-// Add a new todo  
+
 app.post('/todo', function (req, res) {
  
     let task = req.body.task;
  
     if (!task) {
-        return res.status(400).send({ error:true, message: 'Please provide task' });
+        return res.status(400).send({ error:true, message: 'Erro' });
     }
  
     mc.query("INSERT INTO tasks SET ? ", { task: task }, function (error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results, message: 'New task has been created successfully.' });
+        return res.send({ error: false, data: results, message: 'Criado com sucesso.' });
     });
 });
- 
-//  Update todo with id
+
 app.put('/todo', function (req, res) {
  
     let task_id = req.body.task_id;
     let task = req.body.task;
  
     if (!task_id || !task) {
-        return res.status(400).send({ error: task, message: 'Please provide task and task_id' });
+        return res.status(400).send({ error: task, message: 'Digite o ID' });
     }
  
     mc.query("UPDATE tasks SET task = ? WHERE id = ?", [task, task_id], function (error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
+        return res.send({ error: false, data: results, message: 'Atualizado com sucesso.' });
     });
 });
- 
-//  Delete todo
+
 app.delete('/todo/:id', function (req, res) {
  
     let task_id = req.params.id;
  
     mc.query('DELETE FROM tasks WHERE id = ?', [task_id], function (error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results, message: 'Task has been updated successfully.' });
+        return res.send({ error: false, data: results, message: 'Atualizado com sucesso.' });
     });
  
 });
- 
-// all other requests redirect to 404
+
 app.all("*", function (req, res, next) {
-    return res.send('page not found');
+    return res.send('pagina não encotrada');
     next();
 });
  
-// port must be set to 8080 because incoming http requests are routed from port 80 to port 8080
+
 app.listen(8080, function () {
-    console.log('Node app is running on port 8080');
+    console.log('Abra a porta 8080');
 });
  
-// allows "grunt dev" to create a development server with livereload
+
 module.exports = app;
